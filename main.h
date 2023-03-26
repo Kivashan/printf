5a3acabd39fec491d1ce69ce6d13eef54c5eaf63
Albert <albertirura11@gmail.com>
2023-03-26 13:01:46 -0700

    added macro BUFFSIZE and check_buff, and changed function return type from void to int

--- main.h
+++ main.h
@@ -5,18 +5,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
-
+#define BUFFSIZE 1024
 typedef struct printer {
 	char *spec;
-	 void (*case_func)(va_list, char *, unsigned long int *);
+	 int (*case_func)(va_list, char *, unsigned long int *);
 }get;
 
 int _printf(const char *format, ...);
-void copy_char(va_list, char *, unsigned long int *);
-void copy_string(va_list, char *,unsigned long int *);
+int copy_char(va_list, char *, unsigned long int *);
+int copy_string(va_list, char *,unsigned long int *);
 void print_buffer(char *, unsigned long int *);
 int print_percent(const char *, int *, char *, unsigned long int *);
-
+void check_buffer(char *, int *);
 /*int _strlen(char *);
 char *_strcpy(char *, char *);
 int check_end_case(int, const char *);*/
