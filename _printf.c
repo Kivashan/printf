802e41452a3c1bfe98c1ab7c4e15bbfcb9fbcf01
Kivashan <bvkivashan1982@gmail.com>
2023-03-27 02:48:22 -0700

    Removed print_buffer call if %'\0' is true

--- _printf.c
+++ _printf.c
@@ -44,7 +44,6 @@ int _printf(const char * format,...)
 		}
 		if (format[i] == '%' && format[i + 1] == '\0')
 		{
-			print_buffer(buffer, &buff_pos);
 			return (0);
 		}
 		check_buffer(buffer, &buff_pos);
