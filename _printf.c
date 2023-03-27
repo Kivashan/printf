ab4108f9f52ea9e946b2aca67760458a812672c9
Albert <albertirura11@gmail.com>
2023-03-27 15:55:30 -0700

    return NULL if malloc fails

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	buffer = malloc(1024);
 	if (!buffer)
-		return (0);
+		return (NULL);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0')
