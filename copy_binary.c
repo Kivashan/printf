b6951485e744e37971f8fabded55751b59d53e7a
Kivashan <bvkivashan1982@gmail.com>
2023-03-27 08:15:09 -0700

    Changed return type of copy_binary function and copied binary digits directly to buffer instead of via another string

--- copy_binary.c
+++ copy_binary.c
@@ -1,39 +1,36 @@
 #include "main.h"
 
 /**
- * print_binary - converts i to binary
- * @i: Integer to convert to binary
+ * copy_binary - converts i to binary
+ * @ap: va_list, calling va_arg for integer argument
+ * @buffer: buffer to store binary
+ * @bp: position of next empty element in buffer
  *
  * Return: Pointer to a string
  */
 
-char *i_to_binary(int i)
+int  copy_binary(va_list ap, char *buffer, unsigned long int *bp)
 {
-	char *str;
-	int j = i;
+	/*char *str;*/
+	int j = va_arg(ap, int);
 	int len = 0;
-	
+	int len2;
 	/* if i < 0 get absolute value of i */
-	if (i < 0)
-		i = _abs(i);
+	if (j < 0)
+		j = _abs(j);
 
 	/* calculate length for str */
-	for (j = i; j != 0; j /= 2)
+	for ( ; j != 0; j /= 2)
 		len++;
 
-	/* allocate memory to str to hold binary of i */
-	str = malloc(sizeof(char) * (len + 1));
-
-	/* check if malloc failed */
-	if (!str)
-		return (NULL);
-
+	len2 = len;
 	/* Initialise str */
-	for (j = len - 1; j >= 0; j--)
+	for ( ; len >= 0; len--)
 	{
-		int rem = i % 2;
-		i = i / 2;
-		str[j] = rem + 48;
+		int rem = j % 2;
+
+		j = j / 2;
+		buffer[*bp + len] = rem + 48;
 	}
-	return (str);
+	return (len2);
 }
