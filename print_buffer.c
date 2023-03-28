8447b5c8aa36f33bfbd7522d9bedd447003b3890
Albert <albertirura11@gmail.com>
2023-03-27 17:06:44 -0700

    exit(-1) if write fails

--- print_buffer.c
+++ print_buffer.c
@@ -10,7 +10,10 @@
 
 void print_buffer(char *buffer, unsigned long int *buff_pos)
 {
-	write(1, buffer, *buff_pos);
+	int i;
+	i = write(1, buffer, *buff_pos);
+	if (i == -1)
+		exit(-1);
 	free(buffer);
 	*buff_pos = 0;
 }
