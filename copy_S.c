a50149a237559a9cb47ad38e6991c1c3abf9910d
Albert <albertirura11@gmail.com>
2023-03-28 07:24:40 -0700

    copy_S formats special characters

--- copy_S.c
+++ copy_S.c
@@ -10,10 +10,11 @@
  */
 int copy_S(va_list p, char *buffer, unsigned long int *bp)
 {
-	char *str, *tmp, *hex = "0123456789ABCDEF";
+	unsigned char *str, *tmp;
+       char *hex = "0123456789ABCDEF";
 	int i, s, h, rem = 0, k = 0, len = 0, j = 0;
 
-	str = va_arg(p, char *);
+	str = va_arg(p, void *);
 	for (i = 0; str[i]; i++)
 		len++;
 	tmp = malloc(sizeof(char) * len + 5);
@@ -21,14 +22,16 @@ int copy_S(va_list p, char *buffer, unsigned long int *bp)
 		return (-1);
 	for (i = 0; str[i]; i++)
 	{
-		if (((int)str[i] < 32 || (int) str[i] >= 127) && (int)str[i] != 0)
+		if (str[i] >= 127)
+			i = i + 1;
+		if ((str[i] < 32 || str[i] >= 127) && str[i] != 0)
 		{
 			tmp[j] = 92;
 			j = j + 1;
 			tmp[j] = 'x';
 			j = j + 1;
 			h = j + 1;
-			k = (int)str[i];
+			k = str[i];
 			for (s = 0; s < 2; s++, j++, h--)
 			{
 				rem = k % 16;
